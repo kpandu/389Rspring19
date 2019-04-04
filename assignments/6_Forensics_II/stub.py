6ef9d3f16d552239dca322dfd22b15ccdd1224f5
Krishan Panduwawala <kpandu@umd.edu>
2019-04-03 21:10:55 -0400

    hi

--- assignments/6_Forensics_II/stub.py
+++ assignments/6_Forensics_II/stub.py
@@ -2,15 +2,16 @@
 
 import sys
 import struct
-
-
+from datetime import datetime
+offset = 8
+png_count = 0
 # You can use this method to exit on failure conditions.
 def bork(msg):
     sys.exit(msg)
 
 
 # Some constants. You shouldn't need to change these.
-MAGIC = 0x8BADFOOD
+MAGIC = 0x8BADF00D
 VERSION = 1
 
 if len(sys.argv) < 2:
@@ -36,7 +37,114 @@ print("------- HEADER -------")
 print("MAGIC: %s" % hex(magic))
 print("VERSION: %d" % int(version))
 
-# We've parsed the magic and version out for you, but you're responsible for
-# the rest of the header and the actual FPFF body. Good luck!
+timestamp,_ = struct.unpack("<LL", data[offset:offset+8])
+offset = offset + 4
+print("DATE: " + str(datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')))
+author = struct.unpack("8s", data[offset:offset+8])
+offset = offset + 8
+print("Author: " + str(sorted(author)[0].decode('utf-8')))
+
+section_count,_ = struct.unpack("<LL", data[offset:offset+8])
+offset = offset + 4
+section_count = int(section_count)
+print("Section count: %d" % section_count)
+if (section_count < 1):
+    bork("Invalid section offset with size %d" % int(section_count))
+# # We've parsed the magic and version out for you, but you're responsible for
+# # the rest of the header and the actual FPFF body. Good luck!
 
 print("-------  BODY  -------")
+for i in range(section_count):
+    section_type,_ = struct.unpack("<LL", data[offset:offset+8])
+    offset = offset + 4
+    section_type = int(section_type)
+
+    slen,_ = struct.unpack("<LL", data[offset:offset+8])
+    offset = offset + 4
+    
+    if (section_type == 1):
+        print("SECTION_ASCII ")
+        format = str(slen)+ "s"
+        ascii = struct.unpack(format, data[offset:offset+slen])
+        offset += slen  
+        print(ascii[0].decode('utf-8'))
+    elif (section_type == 2): 
+        print("SECTION_UTF8 ")
+        for j in range(slen):
+            utf8 = struct.unpack("<LL", data[offset:offset+8])
+            offset += slen 
+            print(utf8.decode('utf-8'))
+    elif (section_type == 3):
+        print("SECTION_WORDS ")
+        for j in range(slen/4):
+            arr = struct.unpack("s", data[offset:offset+4])
+            offset += 4
+            print(arr[0].decode('utf-8')) 
+    elif (section_type == 4): 
+        print("SECTION_DWORDS ")
+        for j in range(slen/8):
+            arr = struct.unpack("s", data[offset:offset+8])
+            offset += 8
+            print(arr[0].decode('utf-8')) 
+    elif (section_type == 5): 
+        print("SECTION_DOUBLES")
+        for j in range(slen/8):
+            arr = struct.unpack("s", data[offset:offset+8])
+            offset += 8
+            print(arr[0].decode('utf-8')) 
+    elif (section_type == 6):
+        print("SECTION_COORD ")
+        if (slen != 16):
+            bork()
+        coord = struct.unpack("dd", data[offset:offset+16])
+        offset += 16
+        print(coord[0], coord[1])    
+        
+    elif (section_type == 7): 
+        print("SECTION_REFERENCE ")
+        if (slen != 4):
+            bork()
+        indx = struct.unpack("s", data[offset:offset+4])
+        offset += 4
+        print(arr[0].decode('utf-8')) 
+    elif (section_type == 8): 
+        print("SECTION_PNG ")
+        print("PNG STORED IN FILE: " + str(png_count) + "pic.png")
+        format = str(slen)+ "s"
+        file_name = str(png_count) + "pic.png"
+        png_count += 1
+        f = open(file_name, "wb")
+        f.write(bytes([137,80,78,71,13,10,26,10]))
+        string = struct.unpack(format, data[offset:offset+slen])
+        offset += slen
+        string = string[0]
+        f.write(string)
+        f.close()
+    elif (section_type == 9):
+        print("SECTION_GIF87 ")
+        print("GIF STORED IN FILE: " + str(png_count) + "pic.gif")
+        format = str(slen)+ "s"
+        file_name = str(png_count) + "pic.gif"
+        png_count += 1
+        f = open(file_name, "wb")
+        f.write("GIF87a".encode('utf-8'))
+        string = struct.unpack(format, data[offset:offset+slen])
+        offset += slen
+        string = string[0]
+        f.write(string)
+        f.close()  
+    elif (section_type == 10): 
+        print("SECTION_GIF89 ")
+        print("GIF STORED IN FILE: " + str(png_count) + "pic.gif")
+        format = str(slen)+ "s"
+        file_name = str(png_count) + "pic.gif"
+        png_count += 1
+        f = open(file_name, "wb")
+        f.write("GIF89a".encode('utf-8'))
+        string = struct.unpack(format, data[offset:offset+slen])
+        offset += slen
+        string = string[0]
+        f.write(string)
+        f.close()  
+    print('\n')
+    
--- assignments/6_Forensics_II/writeup/README.md
+++ assignments/6_Forensics_II/writeup/README.md
@@ -1,18 +1,62 @@
 # Writeup 6 - Forensics
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION NUMBER HERE*
+Name: Krishan Panduwawala
+Section: 0201
 
 I pledge on my honor that I have not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement: *PUT YOUR NAME HERE*
+Digital acknowledgement: Krishan Panduwawala
 
 ## Assignment Writeup
 
 ### Part 1 (45 Pts)
+1. 142.93.136.81
+2. 
+    i) They are using command injection. This is shown  as the server’s response is “PORT 159,203,113,181,222,226” which meant the hackers were trying multiple ports. Then the server says “PORT command successful” which meant that the hackers were trying command injection on multiple ports until the command succeeded.
 
-*Replace this text with your repsonse to our prompt for part 1!*
+    ii) vsFTPd 3.03 which was at the top of the logs between the hackers and the server. They used this to transfer files.
+3. Their ip is 159.203.113.181 which I found from the source to destination ip addresses since I knew the ip of the website. I found the file that the hackers stole and the destination IP from the server was the hackers’ IP. By putting the ip in Censys.io, I know that the hackers’ are connecting from Clifton, New Jersey
+
+4. They are using port 20. Found when clicking on the packets when the greetz.fpff and find_me.jpeg were transferred between the server and the hackers.
+
+5. 
+    a) A photo called find_me.jpeg. The type of the file is jpeg. I downloaded the raw version of the file using WireShark. Then I was able to change the file type to jpg which let me see the photo. 
+
+    b) The photo was taken at the beach which I found from the photo. I saw a large hand sculpture coming from the sand. I then searched up beaches with hand sculpture coming out of the sand. This brought me to beach called “La Mano (the hand)” which matched the hand statue exactly. The state of the sculpture is in Uruguay and the city is Punta del Este.
+
+    c) It was taken on the Sunday, December 23, 2018 at 05:16:24 PM. I found this through exiftool on the photo.
+
+    d) The back camera of an iPhone 8. This was also found using exiftool on the photo.
+
+    e) 4.5 meters below sea level (exiftool).
+
+6. They left a file called greetz.fpff . I found this through WireShark by filtering for the attackers’ IP and looking at the packets. There were multiple packets with “STOR greetz.fpff” with the destination ip as the website. This was a clear indication that greetz.fpff was being stored. I used the same process to find which file the attackers stole from the website.
+
+7. A countermeasure to prevent this attack from happening is to add a firewall that will control what leaves and enters the file system. A firewalls job is to block unauthorized access from and to a private network. This would prevent hackers from putting files in the network and prevent the hackers accessing secured parts of the network.
+
 
 ### Part 2 (55 Pts)
 
-*Replace this text with your repsonse to our prompt for part 2!*
+1. The file was generated in 2019-03-27 at time 04:15:05
+
+2. The author of the file was fl1nch who we know from previous assignments. 
+
+3. 
+    a) The first section is SECTION_ASCII and it contains “Hey you, keep looking :)”
+    b) The second section is SECTION_COORD and it contains the coordinates “52.336035 4.880673”
+    c) The third section is SECTION_PNG and it contains an image of testudo.
+    d) The fourth section is SECTION_ASCII that contains the flag                                          “ }R983CSMC_perg_tndid_u0y_yllufep0h{-R983CSMC” which in reverse is “CMSC389R-{h0pefully_y0u_didnt_grep_CMSC389R}. “
+    e) The fifth section is SECTION_ASCII and it contains “Q01TQzM4OVIte2hleV9oM3lfeTBVX3lvdV9JX2RvbnRfbGlrZV95b3VyX2Jhc2U2NF9lbmNvZGluZ30=”
+4. 
+    a) CMSC389R-{h0pefully_y0u_didnt_grep_CMSC389R}
+    I found this through parsing greetz.fpff and it was in the 4th section.
+
+    b) CMSC389R-{w31c0me_b@ck_fr0m_spr1ng_br3ak}
+    I found this through parsing a png file in greetz.fpff . Upon opening the file I find a picture of testudo with the flag next to him.
+
+
+
+
+
+
+
